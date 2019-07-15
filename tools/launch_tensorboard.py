795c649a974f38b4f719af0e391ad44e6d281825
Yaroslav Bulatov <yaroslavvb@gmail.com>
2019-07-15 16:44:59 -0700

    update

--- tools/launch_tensorboard.py
+++ tools/launch_tensorboard.py
@@ -5,12 +5,11 @@
 # This will launch r5.large machine on AWS with tensoboard, and print URL
 # in the console
 import ncluster
-ncluster.use_aws()
 
 task = ncluster.make_task('tensorboard',
                           instance_type='r5.large',
                           run_name='tensorboard',
-                          image_name='Deep Learning AMI (Ubuntu) Version 13.0')
+                          image_name='Deep Learning AMI (Ubuntu) Version 23.0')
 task.run('source activate tensorflow_p36')
 task.run(f'tensorboard --logdir={task.logdir}/..', non_blocking=True)
 print(f"Tensorboard at http://{task.public_ip}:6006")
